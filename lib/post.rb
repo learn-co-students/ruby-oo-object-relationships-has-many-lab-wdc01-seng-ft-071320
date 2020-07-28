cc0631b43322c0a924f19b0998fa30acfeba77fc
Rbascoe <roanbascoe5@gmail.com>
2020-07-27 21:13:02 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+class Post
+
+    attr_reader :title
+    attr_accessor :author
+
+
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @author = author
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+       author.name if author
+    end
+
+
+
+
+end
